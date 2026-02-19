20876c7dcb34ff57164de75c9862ff68f3b4d5bd
readywebsites <biz499websites@gmail.com>
2026-02-19 09:48:57 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/https://github.com/readywebsites/avadh-mobile-hori-mask.git/',
+  base: '/https://github.com/readywebsites/avadh-mobile-hori-mask.git  base: '/avadh-mobile-hori-mask/',/',
   plugins: [react()],
 })
